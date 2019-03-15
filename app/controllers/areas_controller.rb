9843dcdb1e8105affe6caf20002c6458b0027571
Naoya-tech <naoya.techwork@gmail.com>
2019-03-15 17:53:09 +0900

    アクションを一部helperに移動

--- app/controllers/areas_controller.rb
+++ app/controllers/areas_controller.rb
@@ -1,4 +1,5 @@
 class AreasController < ApplicationController
+  include AreasHelper
 
   def index
     @areas = Area.all
@@ -12,23 +13,7 @@ class AreasController < ApplicationController
     uri = URI.parse("http://zipcloud.ibsnet.co.jp/api/search?zipcode=#{params[:zipcode]}")
     response = Net::HTTP.get_response(uri)
     @result = JSON.parse(response.body)
-    if @result["message"] == nil
-        @area.zipcode = @result["results"][0]["zipcode"]
-        @area.address1 = @result["results"][0]["address1"]
-        @area.address2 = @result["results"][0]["address2"]
-        @area.address3 = @result["results"][0]["address3"]
-        @area.prefcode = @result["results"][0]["prefcode"]
-        @area.kana1 = @result["results"][0]["kana1"]
-        @area.kana2 = @result["results"][0]["kana2"]
-        @area.kana3 = @result["results"][0]["kana3"]
-        render :new
-    elsif @result["message"] == "パラメータ「郵便番号」の桁数が不正です。"
-      @message = @result["message"]  
-      render :search_get
-    elsif @result["message"] == "必須パラメータが指定されていません。"
-      @message = @result["message"] 
-      render:search_get
-    end
+    api
   end
 
   def create
